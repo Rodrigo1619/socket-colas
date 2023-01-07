cabcf53d7ec1f8d2b8967b135298b2cb48b6bac8
rodrigo1619 <00067920@uca.edu.sv>
2023-01-07 15:22:54 -0600

    feat: showing last ticket and generating new tickets

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Aplicación de socket-colas
+Aplicacion que simula el flujo de atender tickets mediante colas, por ejemplo las que se ven en los bancos
+
+# Que es el nodemon.json?
+Archivo para que no se nos reinicie el servidor cada vez que se genere un ticket, debido a que cada vez que se genera un ticket nuevo
+y se guarda en el json que tenemos como un db, nodemon lo detecta y reinicia el servidor, entonces por eso se ha puesto que 
+ignore todo el .json que este en la carpeta de db
--- public/js/nuevo-ticket.js
+++ public/js/nuevo-ticket.js
@@ -1,3 +1,30 @@
+//Referencias HTML
+const lblNuevoTicket = document.querySelector('#lblNuevoTicket');
+const btnCrear = document.querySelector('button');
 
 
-console.log('Nuevo Ticket HTML');
+const socket = io();
+
+
+
+socket.on('connect', () => {
+    btnCrear.disabled = false //estara habilitado si el servidor esta arriba
+
+});
+
+socket.on('disconnect', () => {
+    btnCrear.disabled = true //si se desconecta se deshabilitara y no se podra dar click
+});
+socket.on('ultimo-ticket', (ultimo)=>{
+    lblNuevoTicket.innerText = 'Ticket ' + ultimo;
+} )
+
+
+btnCrear.addEventListener( 'click', () => {
+
+    socket.emit( 'siguiente-ticket', null, ( ticket ) => {
+        lblNuevoTicket.innerText = ticket;
+    });
+
+}); 
+
